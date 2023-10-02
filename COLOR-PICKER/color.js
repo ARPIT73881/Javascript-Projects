a893e4e2e345908d3496d6cfd1fe97608fa36ac5
ARPIT SHARMA <AS.73881@GMAIL.COM>
2023-10-02 15:07:00 +0530

    first commit

---
+++ COLOR-PICKER/color.js
@@ -0,0 +1,29 @@
+let body = document.querySelector("body");
+
+document.querySelector("#Voilet").addEventListener("click", function () {
+  body.style.backgroundColor = "purple";
+});
+
+document.querySelector("#Indigo").addEventListener("click", function () {
+  body.style.backgroundColor = "indigo";
+});
+
+document.querySelector("#Blue").addEventListener("click", function () {
+  body.style.backgroundColor = "blue";
+});
+
+document.querySelector("#Green").addEventListener("click", function () {
+  body.style.backgroundColor = "green";
+});
+
+document.querySelector("#Yellow").addEventListener("click", function () {
+  body.style.backgroundColor = "yellow";
+});
+
+document.querySelector("#Orange").addEventListener("click", function () {
+  body.style.backgroundColor = "orange";
+});
+
+document.querySelector("#Red").addEventListener("click", function () {
+  body.style.backgroundColor = "red";
+});
